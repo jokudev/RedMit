96acc5743f83a550d0fab55d25a2f1f2bd4d9997
jokudev <git@joku.dev>
2024-06-30 15:10:37 +0200

    Add Default App class

---
+++ src/main/java/dev/joku/redmit/RedMitApplication.java
@@ -0,0 +1,13 @@
+package dev.joku.redmit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedMitApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RedMitApplication.class, args);
+  }
+
+}
